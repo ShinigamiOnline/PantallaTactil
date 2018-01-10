758b77818cf55340cfac72b2176af06c44f1f793
Shinigami <denshaotoko@outlook.com>
2018-01-10 21:13:40 +0100

    commit clase 5.4.1

--- app/src/main/java/pantallatactil/quinteros/jorgeluis/pantallatactil/MainActivity.java
+++ app/src/main/java/pantallatactil/quinteros/jorgeluis/pantallatactil/MainActivity.java
@@ -2,12 +2,49 @@ package pantallatactil.quinteros.jorgeluis.pantallatactil;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView entrada = (TextView) findViewById(R.id.TextViewEntrada);
+        entrada.setOnTouchListener(this);
+    }
+
+    @Override
+    public boolean onTouch(View vista, MotionEvent evento) {
+        TextView salida = (TextView) findViewById(R.id.TextViewSalida);
+        /**
+         * TextView salida = (TextView) findViewById(R.id.TextViewSalida);
+         *  salida.setText("");
+         *   salida.append(evento.getAction() + " , ");
+         */
+        String acciones[] = { "ACTION_DOWN", "ACTION_UP", "ACTION_MOVE", "ACTION_CANCEL","ACTION_OUTSIDE", "ACTION_POINTER_DOWN", "ACTION_POINTER_UP" };
+
+        int accion = evento.getAction();
+
+        int codigoAccion = accion & MotionEvent.ACTION_MASK;
+
+        int iPuntero = (accion & MotionEvent.ACTION_POINTER_INDEX_MASK)
+                >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
+
+        salida.append(acciones[codigoAccion]);
+
+        for (int i = 0; i < evento.getPointerCount(); i++) {
+
+            salida.append(" puntero:" + evento.getPointerId(i) +
+                    " x:" + evento.getX(i) + " y:" + evento.getY(i));
+
+        }
+
+        salida.append("\n");
+
+        return true;
     }
 }
